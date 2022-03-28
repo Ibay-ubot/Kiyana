a46edff86faf3b7479f746104674e06443387eeb
Ibay-ubot <101303882+Ibay-ubot@users.noreply.github.com>
2022-03-28 10:05:40 +0700

    Update alive.py

--- EmikoRobot/modules/alive.py
+++ EmikoRobot/modules/alive.py
@@ -14,11 +14,11 @@ PHOTO = "https://telegra.ph/file/b4e7180e79949bdf0419b.jpg"
 @register(pattern=("/alive"))
 async def awake(event):
   TEXT = f"**Hi [{event.sender.first_name}](tg://user?id={event.sender.id}), I'm Kiyana Robot.** \n\n"
-  TEXT += "✅ **I'm Working Properly** \n\n"
-  TEXT += f"✅ **My Master : [kiyana](https://t.me/Rendra264)** \n\n"
-  TEXT += f"✅ **Library Version :** `{telever}` \n\n"
-  TEXT += f"✅ **Telethon Version :** `{tlhver}` \n\n"
-  TEXT += f"✅ **Pyrogram Version :** `{pyrover}` \n\n"
-  TEXT += "**Thanks for adding me here ✨**"
+  TEXT += "✅ **I'm working properly** \n\n"
+  TEXT += f"✅ **My master : [kiyana](https://t.me/Rendra264)** \n\n"
+  TEXT += f"✅ **Library version :** `{telever}` \n\n"
+  TEXT += f"✅ **Telethon version :** `{tlhver}` \n\n"
+  TEXT += f"✅ **Pyrogram version :** `{pyrover}` \n\n"
+  TEXT += "**Pleased to meet you ✨**"
   BUTTON = [[Button.url("Help", "https://t.me/Rendraonebot?start=help"), Button.url("Support", "https://t.me/kiyanasupport")]]
   await tbot.send_file(event.chat_id, PHOTO, caption=TEXT,  buttons=BUTTON)
