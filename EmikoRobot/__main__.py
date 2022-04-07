b0e76b2866744e50de20ba03fe14f1d0a5c6639e
Ibay-ubot <101303882+Ibay-ubot@users.noreply.github.com>
2022-04-08 01:59:15 +0700

    Update __main__.py

--- EmikoRobot/__main__.py
+++ EmikoRobot/__main__.py
@@ -230,7 +230,7 @@ def start(update: Update, context: CallbackContext):
             )
     else:
         update.effective_message.reply_text(
-            f"**Hello I'm {dispatcher.bot.first_name} How are you🙋**",
+            f"𝗛𝗲𝗹𝗹𝗼, I'm {dispatcher.bot.first_name} How are you🙋",
             parse_mode=ParseMode.HTML
        )
 
