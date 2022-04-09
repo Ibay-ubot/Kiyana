4bc0b81bc3a328a00bd604b3c9d789da089f4173
Ibay-ubot <101303882+Ibay-ubot@users.noreply.github.com>
2022-04-09 18:18:41 +0700

    Update logomaker.py

--- EmikoRobot/modules/logomaker.py
+++ EmikoRobot/modules/logomaker.py
@@ -196,7 +196,7 @@ async def logo_(event):
         await tbot.send_file(
             event.chat_id,
             file=flnme,
-            caption="Logo by [Kiyana Robot](https://t.me/Rendraonebot)",
+            caption="by [Kiyana](https://t.me/Rendraonebot)",
             force_document=False,
         )
         os.remove(flnme)
