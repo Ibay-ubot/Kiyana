af3801623c004f5ea040f24237ee3342678131db
Ibay-ubot <Ibay-ubot@users.noreply.github.com>
2022-04-19 03:05:42 +0000

    Automated code formatting
    Signed-off-by: kennedy-ex <kennedy-ex@users.noreply.github.com>

--- EmikoRobot/modules/alive.py
+++ EmikoRobot/modules/alive.py
@@ -11,14 +11,20 @@ from EmikoRobot import telethn as tbot
 
 PHOTO = "https://telegra.ph/file/b4e7180e79949bdf0419b.jpg"
 
+
 @register(pattern=("/alive"))
 async def awake(event):
-  TEXT = f"**Hi [{event.sender.first_name}](tg://user?id={event.sender.id}), I'm Kiyana Robot.** \n\n"
-  TEXT += "✅ **I'm working properly** \n\n"
-  TEXT += f"✅ **My master : [kiyana](https://t.me/Rendra264)** \n\n"
-  TEXT += f"✅ **Library version :** `{telever}` \n\n"
-  TEXT += f"✅ **Telethon version :** `{tlhver}` \n\n"
-  TEXT += f"✅ **Pyrogram version :** `{pyrover}` \n\n"
-  TEXT += "**Pleased to meet you ✨**"
-  BUTTON = [[Button.url("Help", "https://t.me/Rendraonebot?start=help"), Button.url("Support", "https://t.me/kiyanasupport")]]
-  await tbot.send_file(event.chat_id, PHOTO, caption=TEXT,  buttons=BUTTON)
+    TEXT = f"**Hi [{event.sender.first_name}](tg://user?id={event.sender.id}), I'm Kiyana Robot.** \n\n"
+    TEXT += "✅ **I'm working properly** \n\n"
+    TEXT += f"✅ **My master : [kiyana](https://t.me/Rendra264)** \n\n"
+    TEXT += f"✅ **Library version :** `{telever}` \n\n"
+    TEXT += f"✅ **Telethon version :** `{tlhver}` \n\n"
+    TEXT += f"✅ **Pyrogram version :** `{pyrover}` \n\n"
+    TEXT += "**Pleased to meet you ✨**"
+    BUTTON = [
+        [
+            Button.url("Help", "https://t.me/Rendraonebot?start=help"),
+            Button.url("Support", "https://t.me/kiyanasupport"),
+        ]
+    ]
+    await tbot.send_file(event.chat_id, PHOTO, caption=TEXT, buttons=BUTTON)
